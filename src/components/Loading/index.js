2be6daea6729ce5d0cabda92fc6bbb55695c6870
vitalves <vitalvesilva@gmail.com>
2020-03-10 17:53:24 -0300

    first

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -6,6 +6,10 @@ export default function Loading() {
   return (
     <Container>
       <div className="loading">
+        <span>E</span>
+        <span>D</span>
+        <span>S</span>
+        <span></span>
         <span></span>
         <span></span>
         <span></span>
